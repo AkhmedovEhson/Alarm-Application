30745d46edb3aa4b0d3e3699a816beb22f19cd4c
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-10-17 21:04:32 +0500

    Added AlarmRepository

---
+++ Infrastructure/Persistence/Repositories/AlarmRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class AlarmRepository:Repository<AlarmEntity>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AlarmRepository(ApplicationDbContext context):base(context)
+        {
+            _context = context;
+        }
+    }
+}
