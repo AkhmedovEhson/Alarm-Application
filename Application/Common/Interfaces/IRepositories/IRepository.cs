a66c7170051d29f9644b13a587fd2520de8d824d
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-10-18 08:51:13 +0500

    Added Common services to Application

---
+++ Application/Common/Interfaces/IRepositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces.IRepositories
+{
+    public interface IRepository<T>
+    {
+        public Task<List<T>> GetValuesAsync();
+        public ValueTask<T?> FindAsync(int id);
+        public Task AddAsync(T data);
+        public Task Update(T data);
+        public Task Remove(T data);
+        public Task<int> RemoveId(int id);
+    }
+}
