54f1e4cef1674c08f4c47975f405ecd6fcbf92b6
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-10-19 09:30:18 +0500

    Add ExceptionResult object

---
+++ Domain/Common/ExceptionResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common
+{
+    public class ExceptionResult
+    {
+        public string Type { get; set; } = "";
+        public string Message { get; set; } = "";
+        public int StatusCode { get; set; }
+    }
+}
