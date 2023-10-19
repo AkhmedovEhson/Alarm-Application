be0e0417160b3e4b9c9bc3393f6fbb3aff525c05
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-10-19 09:30:56 +0500

    Add Middlewares

---
+++ WebTimer/Middlewares/PerformanceMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace WebTimer.Middlewares
+{
+    public class PerformanceMiddleware
+    {
+        public RequestDelegate _next;
+
+        public PerformanceMiddleware(RequestDelegate next) {  _next = next; }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Restart();
+
+
+            await _next(context);
+
+            stopwatch.Stop();
+
+            if (stopwatch.ElapsedMilliseconds > 300)
+            {
+                Console.WriteLine($"WebTimer: long running request ... {stopwatch.ElapsedMilliseconds} MS");
+                return;
+            }
+            Console.WriteLine($"WebTimer: Running request ... {stopwatch.ElapsedMilliseconds} MS");
+
+        }
+    }
+}
