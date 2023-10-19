f8c66e2e0712e333a75e1ba08b06df520e108055
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-10-19 10:48:42 +0500

    Worker improvements ...

--- WebTimer/Worker/AlarmJob.cs
+++ WebTimer/Worker/AlarmJob.cs
@@ -1,6 +1,25 @@
-﻿namespace WebTimer.Worker
+﻿using System.Net.Mail;
+
+namespace WebTimer.Worker
 {
-    public class AlarmJob
+    public class AlarmJob : BackgroundService
     {
+        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            while(!cancellationToken.IsCancellationRequested)
+            {
+                // note: 3_600_000 ms is 1 hour
+                var milliseconds = 3_600_000 * (24 - DateTime.UtcNow.Hour);
+
+                await Task.Delay(milliseconds,cancellationToken);
+
+                Console.WriteLine("Hello in 12:00");
+
+
+                // todo: just realize smtp client ... 
+                var smtp = new SmtpClient();
+
+            }
+        }
     }
 }
