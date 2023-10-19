c1799371ba48977b30da651d234d3e552b557a0d
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-10-19 09:29:26 +0500

    Add Error-Types

---
+++ Domain/Common/Errors/Alarm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common.Errors
+{
+    public static partial class Errors
+    {
+        public static partial class Alarm
+        {
+            public static ErrorType NotFound => new ErrorType("Alarm.NotFound", "Alarm not found");
+        }
+
+    }
+}
---
+++ Domain/Common/Errors/ErrorType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common.Errors
+{
+    public struct ErrorType
+    {
+        public ErrorType(string? Type, string Message)
+        {
+            this.Type = Type ?? "Exception.Type";
+            this.Message = Message;
+        }
+        public string Type { get;set; }
+        public string Message { get;set; }
+    }
+}
