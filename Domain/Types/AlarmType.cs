a273a99217ccbd6524d9f4f90edee4a02ff23e8e
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-10-28 15:21:01 +0500

    Response-Types { .... }

---
+++ Domain/Types/AlarmType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Types
+{
+    public class AlarmType
+    {
+        public string Day { get; set; } = string.Empty;
+        public string Hour { get; set; } = string.Empty;
+        public string Minute { get; set; } = string.Empty;
+        public string Second { get; set; } = string.Empty;
+    }
+}
