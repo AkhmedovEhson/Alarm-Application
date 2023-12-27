aa08be7768ed7890d2cab5b53dc1d2486ec6f835
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-27 10:24:03 +0500

    Provided `Response-Types` to response with appropriate props

---
+++ Domain/Common/Types/Deleted.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common.Types
+{
+    public struct Deleted
+    {
+        public int Id { get; set; }
+        public Deleted(int Id) => this.Id = Id;
+    }
+}
---
+++ Domain/Common/Types/Success.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common.Types
+{
+    public struct Success
+    {
+        public Success(int Id) => this.Id = Id;
+        public int Id { get; }
+    }
+}
