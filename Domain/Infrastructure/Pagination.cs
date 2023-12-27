a9e0ff4e14bf58ec44e593e6aece2b3692a085be
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-27 10:55:03 +0500

    New Directory `Infrastructure`, [DB-QUERY] features

---
+++ Domain/Infrastructure/Pagination.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Common.Pagination;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Infrastructure
+{
+    public static class Pagination
+    {
+        public static Task<PagedResponse<T>> PagedListResponse<T>(this IQueryable<T> query, int pageNumber, int pageSize) 
+            => PagedResponse<T>.CreateAsync(query, pageNumber, pageSize);
+    }
+}
