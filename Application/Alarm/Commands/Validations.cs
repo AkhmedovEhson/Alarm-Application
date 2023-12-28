c2d24332942958f60b71e475ce413065982e2c01
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-28 13:45:05 +0500

    Added common validation - methods

---
+++ Application/Alarm/Commands/Validations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Alarm.Commands
+{
+    public static class Validations
+    {
+        public static bool RestrictRingAt(DateTime time)
+        {
+            if (time < DateTime.Now)
+            {
+                return false;
+            }
+            return true;
+
+        }
+    }
+}
