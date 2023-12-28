61994ac643f4b4c1432de6e9945144263b49deaa
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-28 13:44:40 +0500

    Added new validators for `AlarmCommands`

---
+++ Application/Alarm/Commands/CreateAlarmCommand/CreateAlarmCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Alarm.Commands.CreateAlarmCommand
+{
+    /// <summary>
+    /// CreateAlarmCommandValidator - validates props, catches errors if some statements do not match
+    /// </summary>
+    public class CreateAlarmCommandValidator:AbstractValidator<CreateAlarmCommand>
+    {
+        public CreateAlarmCommandValidator()
+        {
+            RuleFor(o => o.RingAt).Must(Validations.RestrictRingAt);
+        }
+        
+
+    }
+}
---
+++ Application/Alarm/Commands/UpdateAlarmCommand/UpdateAlarmCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Alarm.Commands.UpdateAlarmCommand
+{
+    public class UpdateAlarmCommandValidator : AbstractValidator<UpdateAlarmCommand>
+    {
+        public UpdateAlarmCommandValidator()
+        {
+            RuleFor(o => o.RingAt).Must(Validations.RestrictRingAt);
+        }
+    }
+}
