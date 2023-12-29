889a0d45c358080035ba6c2caea7d31ebfd61241
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-30 02:41:54 +0500

    Add bunch of JwtBearer parameters

---
+++ WebTimer/Security/JsonWebTokensConfigurations.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+namespace WebTimer.Security
+{
+
+    public static class JsonWebTokensConfigurations
+    {
+        public static IServiceCollection ProvideJsonWebTokensConfigurations(this IServiceCollection services)
+        {
+            services.AddAuthentication(o =>
+            {
+                o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                o.DefaultForbidScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(config =>
+                {
+                    config.TokenValidationParameters = new()
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = "Issuer",
+                        ValidAudience = "audience",
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(
+                            @"hellowordthisismysecretkeyyoushouldnotreadititisasecret"
+                        )),
+                        NameClaimType = "jti"                        
+                    };
+              });
+            return services;
+        }
+    }
+}
