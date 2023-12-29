e06ddd795881d38811c928fe5e5a232d21288587
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-30 02:44:28 +0500

    Entity configurations

---
+++ Infrastructure/Persistence/Configurations/UserEntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class UserEntityConfiguration : IEntityTypeConfiguration<UserEntity>
+    {
+        public void Configure(EntityTypeBuilder<UserEntity> builder)
+        {
+            builder.Property(o => o.Username).IsRequired();
+            builder.Property(o => o.Password).IsRequired();
+        }
+    }
+}
