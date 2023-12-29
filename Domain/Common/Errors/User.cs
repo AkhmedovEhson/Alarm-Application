606fe292bcf34355eb41f85ebf98b5d41b8609d7
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-29 23:21:22 +0500

    Provided new set of error-responses for user

---
+++ Domain/Common/Errors/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common.Errors
+{
+    public static partial class Errors
+    {
+        public static partial class User
+        {
+            public static ErrorType NotFound => new ErrorType("User.NotFound","User not found");
+        }
+    }
+}
