31609dcc611012e5a6829576a7f12117fdf13ef1
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-30 02:31:14 +0500

    Prop-Type `Password` changed to `string`

--- Domain/Entities/UserEntity.cs
+++ Domain/Entities/UserEntity.cs
@@ -11,6 +11,6 @@ namespace Domain.Entities
     {
         public int Id { get;set; }
         public string Username { get;set; }
-        public byte[] Password { get;set; }
+        public string Password { get;set; }
     }
 }
