3824b2bbfb09a6729723dd01a69cd0603b4396f9
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-30 02:40:35 +0500

    Added `TokenGenerator` to generate JsonWebToken ( hmacSha256 )

---
+++ Domain/Infrastructure/Identity/TokenSecurity.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Infrastructure.Identity
+{
+    public static class TokenGenerator
+    {
+        public static string GenerateToken(UserEntity user)
+        {
+            SigningCredentials credentials = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")),
+                SecurityAlgorithms.HmacSha256);
+
+            var claims = new Claim[]
+            {
+                new Claim("Id",user.Id.ToString()),
+                new Claim("Username",user.Username)
+            };
+
+            var securityToken = new JwtSecurityToken(
+                expires: DateTime.Now.AddDays(10),
+                claims: claims,
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(securityToken);
+        }
+    }
+}
