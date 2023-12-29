171b771ef9ad16e0a4eb53fa8a3ff74e45c9168d
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-29 23:22:08 +0500

    Added `UserRepository`

---
+++ Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Application.Common.Interfaces.IRepositories;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class UserRepository:Repository<UserEntity>,IUserRepository
+    {
+        public UserRepository(ApplicationDbContext db):base(db) { }
+    }
+}
