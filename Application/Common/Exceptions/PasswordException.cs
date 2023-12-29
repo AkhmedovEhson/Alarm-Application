92fc49570a3565a864aa5ef9098b91b396672e43
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-30 02:37:52 +0500

    Added new exception type `PasswordException` occures if password wrong

---
+++ Application/Common/Exceptions/PasswordException.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Common.Errors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Exceptions
+{
+    public class PasswordException:Exception
+    {
+        public ErrorType ErrorType { get; set; }
+        public PasswordException():base() { }
+
+        public PasswordException(ErrorType errorType)
+        {
+            this.ErrorType = errorType;
+        }
+    }
+}
