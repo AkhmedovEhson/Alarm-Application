966ed0e49bcbf9ac2caf2b2181a07772dd0e7fad
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-30 02:30:17 +0500

    Added new ErrorTypes about user

--- Domain/Common/Errors/User.cs
+++ Domain/Common/Errors/User.cs
@@ -11,6 +11,9 @@ namespace Domain.Common.Errors
         public static partial class User
         {
             public static ErrorType NotFound => new ErrorType("User.NotFound","User not found");
+
+            public static ErrorType UsernameNotFound => new ErrorType("User.UsernameNotFound", "User with provided username does not exist");
+            public static ErrorType WrongPassword => new ErrorType("User.WrongPassword","Provided password is wrong");
         }
     }
 }
