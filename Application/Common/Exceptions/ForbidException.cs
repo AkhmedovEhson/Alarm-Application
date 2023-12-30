77e63029f8347f687a653f8f25c41fe627a504c3
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-30 20:50:41 +0500

    Provide new exception `ForbidException`

---
+++ Application/Common/Exceptions/ForbidException.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Common.Errors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Exceptions
+{
+    public class ForbidException: Exception
+    {
+        public ErrorType ErrorType { get; set; }
+        public ForbidException(ErrorType errorType)
+        {
+            ErrorType = errorType;
+        }
+
+        public ForbidException():base() { }
+
+        public ForbidException (string message) : base(message) { }
+    }
+}
