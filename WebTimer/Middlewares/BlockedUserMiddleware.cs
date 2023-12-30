d85ca61e9c4eedd265a787c2e777d46fe13cc357
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-30 20:52:43 +0500

    Added new middleware `BlockedUserMiddleware` checks if user is blocked by a`ADMIN`

---
+++ WebTimer/Middlewares/BlockedUserMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace WebTimer.Middlewares
+{
+    public class BlockedUserMiddleware
+    {
+        private RequestDelegate _next;
+        private IMemoryCache _cache;
+        private ICurrentUserService _currentUserService;
+
+        public BlockedUserMiddleware(
+            RequestDelegate next,
+            IMemoryCache cache,
+            ICurrentUserService currentUserService)
+        {
+            _next = next;
+            _cache = cache;
+            _currentUserService = currentUserService;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            bool isBlocked = _cache.Get($"userId:{_currentUserService.GetId()}") != null;
+            if (isBlocked)
+            {
+                throw new ForbidException();
+            }
+            await _next(context);
+        }
+    }
+}
