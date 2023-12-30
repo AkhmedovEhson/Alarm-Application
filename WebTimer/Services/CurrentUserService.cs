f50184e34f4646e4705be5ae71469184ca188455
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-30 20:51:33 +0500

    Added `CurrentUserService` with bunch of APIs to user's token

---
+++ WebTimer/Services/CurrentUserService.cs
@@ -0,0 +1,23 @@
+﻿
+using Application.Common.Interfaces.IRepositories;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using Application.Common.Exceptions;
+namespace Domain.Infrastructure.Identity
+{
+    public class CurrentUserService:ICurrentUserService
+    {
+        private IHttpContextAccessor _httpContextAccessor;
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public int GetId()
+        {
+            return int.Parse(_httpContextAccessor.HttpContext?.User?.FindFirst(JwtRegisteredClaimNames.Jti)?.Value ?? "0");
+        }
+
+    }
+}
