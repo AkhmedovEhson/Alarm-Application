9e3c8cbe35e4e7da976fffe4a0a278abece6ae4f
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-31 17:05:31 +0500

    Refactor `AlarmJob`

--- WebTimer/Worker/AlarmJob.cs
+++ WebTimer/Worker/AlarmJob.cs
@@ -1,19 +1,25 @@
-﻿using System.Net.Mail;
+﻿using Application.Common.Interfaces.IRepositories;
+using Domain.Entities;
+using System.Net.Mail;
+using System.Text.Json;
 
 namespace WebTimer.Worker
 {
     public class AlarmJob : BackgroundService
     {
+        private ICurrentUserService _currentUserService;
+        public AlarmJob(ICurrentUserService currentUserService) => _currentUserService = currentUserService;
         protected override async Task ExecuteAsync(CancellationToken cancellationToken)
         {
             while(!cancellationToken.IsCancellationRequested)
             {
                 // note: 3_600_000 ms is 1 hour
-                var milliseconds = 3_600_000 * (24 - DateTime.UtcNow.Hour);
+                AlarmEntity alarm = JsonSerializer.Deserialize<List<AlarmEntity>>(_currentUserService.GetAlarms())!.First();
+                var milliseconds = 3_600_000 * (24 - alarm.RingAt.Day);
 
                 await Task.Delay(milliseconds,cancellationToken);
 
-                Console.WriteLine("Hello in 12:00");
+                Console.WriteLine($"Alarm occured :: UserId[{_currentUserService.GetId()}]");
 
             }
         }
