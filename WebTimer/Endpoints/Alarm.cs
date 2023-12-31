25d7fa7605db2d226fd3065a362abbe34548eab3
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-31 12:04:38 +0500

    Fill `<summary/>s` up on endpoints

--- WebTimer/Endpoints/Alarm.cs
+++ WebTimer/Endpoints/Alarm.cs
@@ -15,7 +15,7 @@ using Microsoft.AspNetCore.Cors;
 using Domain.Types;
 using Domain.Common.Types;
 using Domain.Common.Pagination;
-
+using Application.Common.Exceptions;
 namespace WebTimer.Endpoints
 {
     
@@ -24,31 +24,53 @@ namespace WebTimer.Endpoints
     [ApiController]
     public class Alarm : ApiControllerBase
     {
+        /// <summary>
+        /// `GetAsync` - gets all alarms with pagination <seealso cref="PagedResponse{T}"/>
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
         [HttpGet]
         public async Task<PagedResponse<AlarmEntity>> GetAsync([FromQuery] GetAlarmsQuery query)
         {
             return await Sender.Send(query);
         }
-
+        /// <summary>
+        /// `GetByIdAsync` - finds alarm <seealso cref="AlarmEntity"/>, throws <seealso cref="NotFoundException"/> if alarm can not be found
+        /// </summary>
+        /// <param name="query"></param>
+        /// <exception cref="NotFoundException"/>
+        /// <returns></returns>
         [Route("{Id}")]
         [HttpGet]
         public async Task<AlarmEntity> GetByIdAsync([FromRoute] GetAlarmByIdQuery query)
         {
             return await Sender.Send(query);
         }
-
+        /// <summary>
+        /// `PostAsync` - creates alarm
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
         [HttpPost]
         public async Task<AlarmEntity> PostAsync(CreateAlarmCommand command)
         {
             return await Sender.Send(command);
         }
-
+        /// <summary>
+        /// `PutAsync` - updates alarm
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
         [HttpPut]
         public async Task<Success> PutAsync(UpdateAlarmCommand command)
         {
             return await Sender.Send(command);
         }
-
+        /// <summary>
+        /// `DeleteAsync` - deletes alarm
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
         [HttpDelete]
         public async Task<Deleted> DeleteAsync(DeleteAlarmCommand command)
         {
