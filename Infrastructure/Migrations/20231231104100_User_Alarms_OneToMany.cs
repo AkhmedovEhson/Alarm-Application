79ce976e56bcb96c363fa7fbd61d87bcd442607d
Ehson Akhmedov <ahmedovehson2004@gmail.com>
2023-12-31 15:51:54 +0500

    New migration packages `OneToManyRelationship` between alarms and users

---
+++ Infrastructure/Migrations/20231231104100_User_Alarms_OneToMany.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class User_Alarms_OneToMany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserEntityId",
+                table: "Alarms",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alarms_UserEntityId",
+                table: "Alarms",
+                column: "UserEntityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Alarms_Users_UserEntityId",
+                table: "Alarms",
+                column: "UserEntityId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Alarms_Users_UserEntityId",
+                table: "Alarms");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Alarms_UserEntityId",
+                table: "Alarms");
+
+            migrationBuilder.DropColumn(
+                name: "UserEntityId",
+                table: "Alarms");
+        }
+    }
+}
